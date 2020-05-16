58e445db53a2b79702069a6591670e8a6f3c751c
7117 <qinglangsalaheiyou@163.com>
2020-05-16 15:37:07 +0800

    code

--- 004.array.go
+++ 004.array.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var x [5]int;
+	var x [5]int
 
-	x[0]=1;
-	x[1]=2;
+	x[0] = 1
+	x[1] = 2
 
-	fmt.Print(x);
-	fmt.Printf("%v",x);
+	fmt.Print(x)
+	fmt.Printf("%v", x)
 
-	f:=[5]int{1,2,3};
-	fmt.Printf("%v",f);
+	f := [5]int{1, 2, 3}
+	fmt.Printf("%v", f)
 
 }
--- 005.slice.go
+++ 005.slice.go
@@ -3,9 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x:=[5]int{1,2,3,4,5};
-
-	y:=x[1:3];
-
-	fmt.Print(y);
+	x := [5]int{1, 2, 3, 4, 5}
+	y := x[1:3]
+	fmt.Print(y)
 }
