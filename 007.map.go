50e16dcb1423b89123a3758e5586d9f5dabc4095
7117 <qinglangsalaheiyou@163.com>
2020-05-16 15:47:11 +0800

    func

--- 007.map.go
+++ 007.map.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var aa map[string]int;
-	aa = make(map[string]int);
+	var aa map[string]int
+	aa = make(map[string]int)
 
-	aa["a"] = 10;
-	aa["b"] = 20;
+	aa["a"] = 10
+	aa["b"] = 20
 
-	fmt.Printf("%v", aa);
+	fmt.Printf("%v", aa)
 
-
-	bb := make(map[string]int);
-	bb["f"]=10;
-	bb["w"]=20;
-	fmt.Printf("%v", bb);
+	bb := make(map[string]int)
+	bb["f"] = 10
+	bb["w"] = 20
+	fmt.Printf("%v", bb)
 
 }
