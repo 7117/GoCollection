b5fa4ffb1a1a6c36a436ab6a18e57938625b4138
7117 <qinglangsalaheiyou@163.com>
2020-05-17 22:17:13 +0800

    buff channel

---
+++ 031.no buff channel.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func test(ch chan int) {
+	fmt.Print("test1")
+	ch <- 1
+	fmt.Print("test2")
+}
+
+func main() {
+	ch := make(chan int,0)
+	go test(ch)
+	time.Sleep(3 * time.Second)
+	fmt.Print("main end")
+	<-ch
+	time.Sleep(3 * time.Second)
+
+}
