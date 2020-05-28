4cba4b3b9678b6792845927b306dbedca1a8928f
7117 <qinglangsalaheiyou@163.com>
2020-05-28 20:36:53 +0800

    code

--- video/scheduler/main.go
+++ video/scheduler/main.go
@@ -1,1 +1,22 @@
 package scheduler
+
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"goPractice/video/scheduler/task"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/videorecord/:vid-id", vidDelRecHandler)
+
+	return router
+}
+
+func main() {
+	// 启动taskrunner
+	go task.Start()
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)
+}
