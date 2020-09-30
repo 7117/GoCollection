678955947930c97f58b9f4222084e21603719140
7117 <sunxiao789@foxmail.com>
2020-09-30 22:46:04 +0800

    var

---
+++ case02/002.var.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var a int = 2
+
+func main() {
+	var x int
+	x = 1
+
+
+	var r int = 8
+	d := 4
+	fmt.Println(x)
+	fmt.Println(a)
+	fmt.Println(d)
+	fmt.Println(r)
+
+}
