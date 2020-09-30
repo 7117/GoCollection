38aab9fecc49865c392af530562bd7192757455b
7117 <qinglangsalaheiyou@163.com>
2020-09-30 23:02:02 +0800

    arr

---
+++ case02/004.complex.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x complex64 = 5+2i
+	fmt.Println(x)
+}
---
+++ case02/005.arr.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr [4]int
+	arr[0] = 1
+	arr[3] = 11
+	fmt.Println(arr)
+	fmt.Printf("%v",arr)
+}
