b649a1670ac8f8f51dbb774e0a12a7dafa1fce4e
7117 <qinglangsalaheiyou@163.com>
2020-09-30 22:54:26 +0800

    const

--- case02/002.var.go
+++ case02/002.var.go
@@ -7,7 +7,7 @@ var a int = 2
 func main() {
 	var x int
 	x = 1
-	
+
 	var r int = 8
 	d := 4
 	fmt.Println(x)
---
+++ case02/003.const.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const PI = 3.1415926
+	const hello string = "zh"
+
+	fmt.Println(PI)
+	fmt.Println(hello)
+}
