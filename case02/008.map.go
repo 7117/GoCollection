455ee1355ca4698e2811b8da76b4cef4ba07b07c
7117 <qinglangsalaheiyou@163.com>
2020-09-30 23:35:39 +0800

    map

---
+++ case02/008.map.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var stu map[string]float32
+	stu = make(map[string]float32)
+	stu["zs"] = 11.11
+	fmt.Printf("%v",stu)
+
+	fmt.Println()
+
+	tea := make(map[string]float32)
+	tea["te"] = 22.22
+	fmt.Printf("%v",tea)
+}
