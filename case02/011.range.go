ce68a84eff6abfd669c4e0202faef4cfb5a8a3f0
7117 <qinglangsalaheiyou@163.com>
2020-10-01 00:05:57 +0800

    range

---
+++ case02/011.range.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [5]int{0,1,2,3,4}
+
+	for i,v := range arr{
+		fmt.Println(i,v)
+	}
+
+	x := make(map[string]float32)
+	x["z"] = 11.11
+	x["y"] = 22.22
+	x["r"] = 33.33
+
+	for i,v := range x{
+		fmt.Println(i,v)
+	}
+}
