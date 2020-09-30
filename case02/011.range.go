454ecac7477709a3f4651a0c220049d3e8897fba
7117 <qinglangsalaheiyou@163.com>
2020-10-01 00:10:52 +0800

    range

--- case02/011.range.go
+++ case02/011.range.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	arr := [5]int{0,1,2,3,4}
-
 	for i,v := range arr{
 		fmt.Println(i,v)
 	}
@@ -13,8 +12,15 @@ func main() {
 	x["z"] = 11.11
 	x["y"] = 22.22
 	x["r"] = 33.33
-
 	for i,v := range x{
 		fmt.Println(i,v)
 	}
+
+
+	r := "asd";
+	for _,v := range r{
+		fmt.Printf("%c\n",v)
+	}
+
+
 }
