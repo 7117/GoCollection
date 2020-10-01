7604ade08efe4189813bc0408f01cd35c0b81a60
7117 <qinglangsalaheiyou@163.com>
2020-10-01 14:21:00 +0800

    引用方法

---
+++ case02/023.new.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Personone struct {
+	name string
+	age  int
+}
+
+type Stuone struct {
+	Personone
+	specially string
+}
+
+func (person Personone) getNameadnAge() (string, int) {
+	return person.name,person.age
+}
+
+func main() {
+	studentone := new(Stuone)
+	studentone.name = "zs";
+	studentone.age = 12;
+	name,age := studentone.getNameadnAge()
+	fmt.Println(name,age)
+}
