962b6e4ef118c34ee4e36dca8663eecafd20be39
7117 <qinglangsalaheiyou@163.com>
2020-10-02 11:37:11 +0800

    jsn=on obj

--- case02/039.json obj.go
+++ case02/039.json obj.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Student struct {
+	Name string `json:"aa"`
+	Age int `json:"bb"`
+}
+
 func main() {
-	
+
+	stu := Student{"a", 20}
+
+	res,err:= json.Marshal(stu);
+
+	if err!=nil{
+		fmt.Println(err)
+	}
+	fmt.Println(string(res))
+
+	//对json进行解码
+	var dd interface{}
+	json.Unmarshal([]byte(res),&dd)
+	fmt.Println(dd)
 }
 
