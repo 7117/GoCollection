68c81dd5c0e53106cd1a11368b58c1c970ca2d0a
7117 <qinglangsalaheiyou@163.com>
2020-10-02 11:40:56 +0800

    md5

--- case02/040.unjson obj.go
+++ case02/040.unjson obj.go
@@ -14,10 +14,16 @@ func main() {
 
 	stu := Student{"a", 20}
 
-	res, err := json.Marshal(stu);
-	if err != nil {
+	res,err:= json.Marshal(stu);
+
+	if err!=nil{
 		fmt.Println(err)
 	}
 	fmt.Println(string(res))
+
+	//对json进行解码
+	var dd interface{}
+	json.Unmarshal([]byte(res),&dd)
+	fmt.Println(dd)
 }
 
