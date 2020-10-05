4a57e77b4748710b703ac8644d3fc278d6f9ef74
7117 <qinglangsalaheiyou@163.com>
2020-10-05 18:41:53 +0800

    model

---
+++ beego02/controllers/model.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type ModelController struct {
+	beego.Controller
+}
+
+func (this *ModelController) Test() {
+	
+}
+
+func (this *ModelController) Login() {
+
+}
