97b459fe2d367140531f726ada5a7785bdc8d95c
7117 <qinglangsalaheiyou@163.com>
2020-10-07 13:50:03 +0800

    sql准备

---
+++ spider02/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "goPractice/spider02/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ spider02/routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"goPractice/spider02/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
