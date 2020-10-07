2fd52475cc2fbb1ea6707ae140e128a806f6c39a
7117 <qinglangsalaheiyou@163.com>
2020-10-07 21:39:05 +0800

    fmt.Sprontf

--- spider02/controllers/spider.go
+++ spider02/controllers/spider.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/httplib"
 	"goPractice/spider02/models"
@@ -16,18 +17,22 @@ func (c *SpiderController) CrawlMovie() {
 	sHtml := httplib.Get(sUrl)
 	sHtmls, _ := sHtml.String()
 
-	c.Ctx.WriteString(models.GetMovieDirector(sHtmls)+ "|")
-	c.Ctx.WriteString(models.GetMovieName(sHtmls)+ "|")
-	c.Ctx.WriteString(models.GetMovieMainCharacters(sHtmls)+ "|")
-	c.Ctx.WriteString(models.GetMovieGrade(sHtmls)+ "|")
-	c.Ctx.WriteString(models.GetMovieGenre(sHtmls)+ "|")
-	c.Ctx.WriteString(models.GetMovieOnTime(sHtmls)+ "|")
-	c.Ctx.WriteString(models.GetMovieRunningTime(sHtmls))
+	var movieInfo models.MovieInfo
+
+	movieInfo.Movie_name = models.GetMovieDirector(sHtmls)
+	movieInfo.Movie_director = models.GetMovieName(sHtmls)
+	movieInfo.Movie_main_character = models.GetMovieMainCharacters(sHtmls)
+	movieInfo.Movie_grade = models.GetMovieGrade(sHtmls)
+	movieInfo.Movie_type = models.GetMovieGenre(sHtmls)
+	movieInfo.Movie_on_time = models.GetMovieOnTime(sHtmls)
+	movieInfo.Movie_span = models.GetMovieRunningTime(sHtmls)
+
+	id, err := models.AddMovie(&movieInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.Ctx.WriteString(fmt.Sprintf("%v", id))
 
 }
-
-
-
-
-
-
